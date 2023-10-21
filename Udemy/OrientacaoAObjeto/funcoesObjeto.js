24842764c9bf85a5437f30d3c63a309fd154b811
Pedro Vinícius <pedroviniciussouza24@gmail.com>
2023-10-21 11:51:48 -0300

    funcoes de objeto.

---
+++ Udemy/OrientacaoAObjeto/funcoesObjeto.js
@@ -0,0 +1,31 @@
+const pessoa = {
+    nome: 'Rebeca',
+    idade: 2,
+    peso: 13,
+}
+
+
+Object.entries(pessoa).forEach(([chave, valor]) =>{
+    console.log(`${chave}: ${valor}`)
+})
+
+
+Object.defineProperty(pessoa, 'dataDeNascimento', {
+    enumerable: false,
+    writable: false,
+    value: '01/01/2019'
+}) 
+
+
+pessoa.dataDeNascimento = '01/01/2017'
+console.log(pessoa.dataDeNascimento)
+
+console.log(Object.keys(pessoa))
+
+//Object.assingn (ECMAScript 2015)
+
+const dest = {a: 1}
+const a1 = {b: 2}
+const a2 = {c:3, a:4}
+
+const obj = Object.assign(dest, a1, a2)
