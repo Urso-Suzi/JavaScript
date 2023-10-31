d6124347ac45011ce684bd3cdf8437167189b37f
Pedro Vinícius <pedroviniciussouza24@gmail.com>
2023-10-31 05:59:58 -0300

    aplicando em projetos reais

--- Udemy/Node/funcionarios/funcionarios.js
+++ Udemy/Node/funcionarios/funcionarios.js
@@ -2,6 +2,17 @@ const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
 axios.get(url).then(response => {
-    const funcionarios = response.data
-    console.log(funcionarios)
+    let funcionarios = response.data
+
+    const pais = func => func.pais == 'China'
+    const genero = func => func.genero == 'M'
+  
+   const funcChinesas = funcionarios.filter(pais).filter(genero)
+
+    console.log(funcChinesas.sort())
+   
 })
+
+
+
+
