229b457003c7d543f712456c428c1e32fb7ab143
Pedro Vinícius <pedroviniciussouza24@gmail.com>
2023-11-05 17:50:30 -0300

    server

--- Udemy/Node/Projeto/src/servidor.js
+++ Udemy/Node/Projeto/src/servidor.js
@@ -3,6 +3,7 @@ const porta = 3003
 const express = require('express')
 const app = express()
 
+
 app.get('/produtos', (req, res, next) =>{
     res.send({nome: 'Notebook', preco: 1234.45}) // converter para Json
 })
