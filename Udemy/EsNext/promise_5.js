8df5374372caa4f4c48940288a393a0fef5d27c9
Pedro Vinícius <pedroviniciussouza24@gmail.com>
2023-11-20 06:02:33 -0300

    resolve & reject

--- Udemy/EsNext/promise_5.js
+++ Udemy/EsNext/promise_5.js
@@ -1,14 +1,26 @@
 function funcionarOuNao(valor, chanceDeErro) {
     return new Promise ((resolve, reject) => {
-        if(Math.random() < chanceDeErro) {
+       try { 
+            con.log('temp')
+            if(Math.random() < chanceDeErro) {
             reject('Ocorreu um erro')
-        } else {
+         } else {
             resolve(valor)
-        }
-    })
+          }  
+    
+         } catch(e) {
+            reject(e)
+         }
+    }) 
 }
 
 
-funcionarOuNao('Testando', 0.1)
+funcionarOuNao('Testando...', 0.9)
     .then(v => console.log(`Valor: ${v}`))
+    .then(
+        v => consol.log(v),
+        //err => console.log(`Erro Esp.: ${err}`)
+        )
+    .then(() => console.log('Quase Fim!'))
     .catch(err => console.log(`Erro: ${err}`))
+    .then(() => console.log('Fim!'))
