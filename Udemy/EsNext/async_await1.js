a22e6449f7fd271ce4cf1f9f6187ef2b4ab9d19e
Pedro Vinícius <pedroviniciussouza24@gmail.com>
2023-11-21 05:47:40 -0300

    async await

--- Udemy/EsNext/async_await1.js
+++ Udemy/EsNext/async_await1.js
@@ -9,12 +9,17 @@ function retornarValor() {
     return new Promise(resolve => {
         setTimeout(() => {resolve(10)}, 5000)
     })
-}
+} //aqui fazemos uma função promise que espera 5 sec, antes de retornar a resposta 
+
+
+async function executarRapido() {
+    return 20
+} //neste caso a promise já está resolvida e não precisa esperar
 
 async function executar() {
 
-    let valor = await retornarValor()
-
+    let valor = await retornarValor() // executarRapido()
+// é necessário usar o await, para que a função promise, consiga acessar os valor contigos, do contrário ela só vai acessar o tipo 
     await Esperarpor(2000)    
     console.log(`Async/Await ${valor}...`)
     
@@ -23,6 +28,15 @@ async function executar() {
     
     await Esperarpor(2000)
     console.log(`Async/Await ${valor + 2}...`)
+
+    return valor + 3
 }
 
-executar()
+async    function executarDeVerdade(){
+    const valor = await executar()
+    console.log(valor)
+}
+
+executarDeVerdade()
+
+
